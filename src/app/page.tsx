089534e2c97448497b74c7514bf79d8f36390483
Nielleeeee <jandnllplz@gmail.com>
2024-03-09 04:04:50 +0800

    chore: UI adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import { UserButton, currentUser } from "@clerk/nextjs";
+import Header from "@/components/layout/header";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+
   return (
-    <main>
-      <h1>Welcome, Chuwie</h1>
+    <main className="min-h-screen bg-slate-700">
+      <Header />
+      <h1>
+        Welcome, <span className="text-purple-100">{user?.firstName}</span>
+      </h1>
+
+      {/* Post here */}
     </main>
   );
 }
