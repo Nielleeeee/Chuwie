3cccad6abd013521b668f06873c489093be793fe
Nielleeeee <jandnllplz@gmail.com>
2024-03-09 04:05:19 +0800

    feat: added clerk loading state for sign in

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,33 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
+import { GradiantLoader } from "@/components/loaders/loader";
 
 export default function Login() {
   return (
     <section>
       <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
         <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div className="flex flex-col lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
             <Image
               src={"/logo/chuwie-logo-no-bg.png"}
               alt="chuwie logo"
               width={100}
               height={100}
-              className="mx-auto"
+              className="mx-auto mb-12"
             />
 
-            <div className="mt-12 flex flex-col items-center">
-              <div className="mt-8">
-                <SignIn appearance={{
-                  elements: {
-                    socialButtonsBlockButton: "bg-purple-100",
-                    formButtonPrimary:
-                      "bg-purple-400 hover:bg-purple-500 text-sm normal-case",
-                    footerActionLink: "text-purple-700"
-                  },
-                }} />
-              </div>
+            <div className="my-auto flex flex-col items-center justify-center">
+              <ClerkLoading>
+                <GradiantLoader />
+              </ClerkLoading>
+
+              <ClerkLoaded>
+                <SignIn
+                  appearance={{
+                    elements: {
+                      socialButtonsBlockButton: "bg-purple-100",
+                      formButtonPrimary:
+                        "bg-purple-400 hover:bg-purple-500 text-sm normal-case",
+                      footerActionLink: "text-purple-700",
+                    },
+                  }}
+                />
+              </ClerkLoaded>
             </div>
           </div>
 
