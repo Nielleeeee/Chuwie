61c95f2b57625859ed09f4aff4545dd8d53c162c
Nielleeeee <jandnllplz@gmail.com>
2024-03-10 03:37:38 +0800

    chore: changed ui structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { UserButton, currentUser } from "@clerk/nextjs";
 import Header from "@/components/layout/header";
+import SamplePost from "@/components/samplePost";
+import CreatePost from "@/components/post/createPost";
 
 export default async function Home() {
   const user = await currentUser();
@@ -8,11 +10,18 @@ export default async function Home() {
   return (
     <main className="min-h-screen bg-slate-700">
       <Header />
-      <h1>
-        Welcome, <span className="text-purple-100">{user?.firstName}</span>
-      </h1>
 
-      {/* Post here */}
+      <section className="flex flex-col w-full max-w-3xl mx-auto px-4 md:px-8">
+        <div className="relative py-5">
+          <h1 className="text-purple-100 mb-10 text-xl">
+            Welcome, <span className="font-semibold">{user?.firstName}!</span>
+          </h1>
+          
+          <CreatePost />
+        </div>
+
+        <SamplePost />
+      </section>
     </main>
   );
 }
