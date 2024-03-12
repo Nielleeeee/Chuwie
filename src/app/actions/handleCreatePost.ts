cb4ca17fe91c32076b92a22a27902db52a1af0de
Nielleeeee <jandnllplz@gmail.com>
2024-03-13 04:28:22 +0800

    chore: testing upload using server action

--- src/app/actions/handleCreatePost.ts
+++ src/app/actions/handleCreatePost.ts
@@ -2,13 +2,9 @@
 
 import { v2 as cloudinary } from "cloudinary";
 
-export async function handleCreatePost(data?: any) {
+export async function handleCreatePost(values: CreatePost) {
   try {
-    // Simulate an asynchronous operation (e.g., API call) with a timeout
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log(data);
-    // Assuming the operation was successful, you can return some data or a success message
-    return { success: true, message: "Post created successfully" };
+    console.log(values);
   } catch (error) {
     console.error("Failed to create post: ", error);
     throw error;
