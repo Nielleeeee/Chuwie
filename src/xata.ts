98812af172bb7cf6bc6f643a8de18d5e664a6c08
Nielleeeee <jandnllplz@gmail.com>
2024-03-14 04:03:19 +0800

    chore: sync database schema

--- src/xata.ts
+++ src/xata.ts
@@ -6,18 +6,47 @@ import type {
   XataRecord,
 } from "@xata.io/client";
 
-const tables = [] as const;
+const tables = [
+  {
+    name: "Post",
+    columns: [
+      { name: "user_id", type: "string", unique: true },
+      { name: "author_username", type: "string" },
+      { name: "content", type: "text" },
+      { name: "author_fullname", type: "string" },
+      { name: "media", type: "json" },
+    ],
+  },
+  {
+    name: "Comment",
+    columns: [
+      { name: "post_id", type: "string" },
+      { name: "content", type: "string" },
+      { name: "user_id", type: "string" },
+    ],
+  },
+] as const;
 
 export type SchemaTables = typeof tables;
 export type InferredTypes = SchemaInference<SchemaTables>;
 
-export type DatabaseSchema = {};
+export type Post = InferredTypes["Post"];
+export type PostRecord = Post & XataRecord;
+
+export type Comment = InferredTypes["Comment"];
+export type CommentRecord = Comment & XataRecord;
+
+export type DatabaseSchema = {
+  Post: PostRecord;
+  Comment: CommentRecord;
+};
 
 const DatabaseClient = buildClient();
 
 const defaultOptions = {
   databaseURL:
     "https://Jan-Danielle-Plaza-s-workspace-oti195.us-east-1.xata.sh/db/chuwie",
+  branch: "main",
 };
 
 export class XataClient extends DatabaseClient<DatabaseSchema> {
