0e4e71ebcb58685f1f17fd7038630159e3a81a0b
Nielleeeee <jandnllplz@gmail.com>
2024-03-15 05:10:30 +0800

    chore: types updates

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,17 @@
+interface PostData {
+  author_fullname: string;
+  author_username: string | null;
+  content: string;
+  id: string;
+  media: string[];
+  user_id: string | null;
+  xata: {
+    createdAt: string;
+    updatedAt: string;
+    version: number;
+  };
+}
+
 interface CreatePost {
   content: string;
   media: [];
