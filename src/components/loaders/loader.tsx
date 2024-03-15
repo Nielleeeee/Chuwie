4282754d0f7ee725d93841a002ca263609d081e6
Nielleeeee <jandnllplz@gmail.com>
2024-03-16 04:41:03 +0800

    chore: added loader more post

--- src/components/loaders/loader.tsx
+++ src/components/loaders/loader.tsx
@@ -11,3 +11,13 @@ export const GradiantLoader = () => {
 export const AvatarLoader = () => {
   return <div className="rounded-full w-8 h-8 animate-pulse bg-gray-400"></div>;
 };
+
+export const PostLoader = () => {
+  return (
+    <div className="flex flex-row gap-2">
+      <div className="w-4 h-4 rounded-full bg-purple-400 animate-bounce"></div>
+      <div className="w-4 h-4 rounded-full bg-purple-400 animate-bounce [animation-delay:-.3s]"></div>
+      <div className="w-4 h-4 rounded-full bg-purple-400 animate-bounce [animation-delay:-.5s]"></div>
+    </div>
+  );
+};
