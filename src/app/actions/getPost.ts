e34db09953b102a2bd94bd818dcfcadbbd4760ae
Nielleeeee <jandnllplz@gmail.com>
2024-03-18 02:27:20 +0800

    chore: added get initial post for react query

--- src/app/actions/getPost.ts
+++ src/app/actions/getPost.ts
@@ -4,7 +4,30 @@ import { getXataClient } from "@/xata";
 
 const xataClient = getXataClient();
 
-export const getAllPost = async (page: number, pageSize: number = 3) => {
+export const getInitialPost = async () => {
+  try {
+    const pageSize = 3;
+    const post = await xataClient.db.Post.sort(
+      "xata.createdAt",
+      "desc"
+    ).getPaginated({ pagination: { size: pageSize } });
+
+    // Preprocess posts
+    const processedPosts = post.records.map((record) => {
+      // Convert xata to plain object
+      const xata = { ...record.xata };
+      return { ...record, xata };
+    });
+
+    const hasNextPage = post.hasNextPage();
+    return { posts: processedPosts, hasNextPage };
+  } catch (error) {
+    console.error("Failed to fetch post: ", error);
+    throw error;
+  }
+}
+
+export const getAllPost = async (page: number = 2, pageSize: number = 3) => {
   try {
     const offset = (page - 1) * pageSize;
     const post = await xataClient.db.Post.sort(
