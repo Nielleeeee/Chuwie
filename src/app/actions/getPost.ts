bc40cc07ea68be330964c78de60ba27c11048406
Nielleeeee <jandnllplz@gmail.com>
2024-03-20 03:00:09 +0800

    added get more post & get all post fetch function

--- src/app/actions/getPost.ts
+++ src/app/actions/getPost.ts
@@ -25,11 +25,34 @@ export const getInitialPost = async () => {
     console.error("Failed to fetch post: ", error);
     throw error;
   }
-}
+};
 
-export const getAllPost = async (page: number = 2, pageSize: number = 3) => {
+export const getMorePost = async (page: number = 2, pageSize: number = 3) => {
   try {
     const offset = (page - 1) * pageSize;
+    const post = await xataClient.db.Post.sort(
+      "xata.createdAt",
+      "desc"
+    ).getPaginated({ pagination: { size: pageSize, offset } });
+
+    // Preprocess posts
+    const processedPosts = post.records.map((record) => {
+      // Convert xata to plain object
+      const xata = { ...record.xata };
+      return { ...record, xata };
+    });
+
+    const hasNextPage = post.hasNextPage();
+    return { posts: processedPosts, hasNextPage };
+  } catch (error) {
+    console.error("Failed to fetch post: ", error);
+    throw error;
+  }
+};
+
+export const getAllPost = async ({ pageParam = 1 }, pageSize = 3) => {
+  try {
+    const offset = (pageParam - 1) * pageSize;
     const post = await xataClient.db.Post.sort(
       "xata.createdAt",
       "desc"
