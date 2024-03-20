0dc778927fc004959d6ebdcaeca13b3aad498333
Nielleeeee <jandnllplz@gmail.com>
2024-03-21 04:00:23 +0800

    refactor: fixed return value for next page param

--- src/app/data/get-post.ts
+++ src/app/data/get-post.ts
@@ -13,8 +13,9 @@ export const useGetAllPost = () => {
     queryKey: ["allPosts"],
     queryFn: ({ pageParam = 1 }) => getAllPost({ pageParam }) as any,
     initialPageParam: 1,
-    getNextPageParam: (lastPage) => {
-      return lastPage;
+    getNextPageParam: (lastPage, allPages) => {
+      const hasNextPage = allPages[allPages.length - 1]?.hasNextPage;
+      return hasNextPage ? allPages.length + 1 : undefined;
     },
   });
 };
