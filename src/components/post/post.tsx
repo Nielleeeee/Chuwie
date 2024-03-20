e80ba867dfc0a1bfb63813a0add1c9d1455fde86
Nielleeeee <jandnllplz@gmail.com>
2024-03-21 04:03:27 +0800

    feat: used infinite scroll from react query

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,29 +1,47 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
-import { useGetInitialPost, useGetAllPost } from "@/app/data/get-post";
+import { useGetAllPost } from "@/app/data/get-post";
 import { TransitionMoveUp } from "@/components/animation/transition";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getAllPost } from "@/app/actions/getPost";
+import { useInView } from "react-intersection-observer";
+import { PostLoader } from "@/components/loaders/loader";
+import DateFormat from "@/app/lib/date-format";
 
 export default function Post() {
-  const { data } = useGetAllPost();
+  const { ref, inView } = useInView();
+  const {
+    data,
+    status,
+    error,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useGetAllPost();
 
-  // const { data } = useGetInitialPost();
+  const allPosts = data?.pages.flatMap((page) => page.posts) || [];
 
-  // const initialPost = data?.posts;
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView, hasNextPage]);
 
-  console.log(data);
-
-  return (
+  return status === "pending" ? (
+    <PostLoader />
+  ) : status === "error" ? (
+    <p>Error: {error.message}</p>
+  ) : (
     <section className="flex flex-col">
-      {/* {initialPost?.length !== 0 &&
-        initialPost?.map((post: any, index: number) => (
+      {allPosts?.length !== 0 &&
+        allPosts?.map((post: any, index: number) => (
           <TransitionMoveUp key={index}>
             <div className="rounded-md bg-white p-4 my-4 shadow-md">
               <h3>{post.author_fullname}</h3>
               <p className="mb-4">{post.content}</p>
+              <DateFormat date={post.xata.createdAt} />
 
               <figure className="flex flex-row gap-2 w-full overflow-hidden">
                 {post.media.length !== 0 &&
@@ -40,7 +58,20 @@ export default function Post() {
               </figure>
             </div>
           </TransitionMoveUp>
-        ))} */}
+        ))}
+
+      {hasNextPage || isFetchingNextPage ? (
+        <div
+          ref={ref}
+          className="w-full py-10 flex justify-center items-center"
+        >
+          <PostLoader />
+        </div>
+      ) : (
+        <div className="w-full text-white text-xl font-medium py-10 flex justify-center items-center">
+          No more post available 😔
+        </div>
+      )}
     </section>
   );
 }
