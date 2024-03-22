9227193fb9478dd8b4cb2380d75dbbf7180543a2
Nielleeeee <jandnllplz@gmail.com>
2024-03-23 04:57:37 +0800

    feat: added delete post component

--- src/components/post/postList.tsx
+++ src/components/post/postList.tsx
@@ -2,14 +2,25 @@ import React from "react";
 import Image from "next/image";
 import { TransitionMoveUp } from "@/components/animation/transition";
 import DateFormat from "@/app/lib/date-format";
+import DeletePost from "@/components/post/deletePost";
+import { useAuth } from "@clerk/nextjs";
 
 export default function PostList({ PostData }: any) {
+  const { userId } = useAuth();
+
   return (
     <>
       {PostData?.length !== 0 &&
         PostData?.map((post: any, index: number) => (
           <TransitionMoveUp key={index}>
-            <div className="rounded-md bg-white p-4 my-4 shadow-md">
+            <div className="relative rounded-md bg-white p-4 my-4 shadow-md">
+              {userId == post.user_id && (
+                <DeletePost
+                  postId={post.id}
+                  className="absolute top-4 right-4"
+                />
+              )}
+
               <h3>{post.author_fullname}</h3>
               <DateFormat date={post.xata.createdAt} />
               <p className="mb-4">{post.content}</p>
