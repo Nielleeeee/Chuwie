d5be5457f1090940853499e71ebd249cb321d823
Nielleeeee <jandnllplz@gmail.com>
2024-04-05 05:14:22 +0800

    refactor: fixed some UI issue & added lighthouse

--- src/components/post/mediaList.tsx
+++ src/components/post/mediaList.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Image from "next/image";
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Lightbox from "yet-another-react-lightbox";
 
+import "yet-another-react-lightbox/styles.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 export default function MediaList({ postData }: any) {
   return (
-    <figure className="w-full px-2 pb-4">
+    <figure className="w-full">
       <Swiper
         modules={[Navigation, Pagination]}
         spaceBetween={50}
@@ -18,12 +20,11 @@ export default function MediaList({ postData }: any) {
         pagination={{ clickable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
-      
       >
         {postData.media.length !== 0 &&
           postData.media.map((item: { secure_url: string }, key: number) => (
             <SwiperSlide key={key}>
-              <div className="w-full flex justify-center items-center overflow-hidden p-">
+              <div className="w-full flex justify-center items-center overflow-hidden">
                 <Image
                   src={item.secure_url}
                   alt={postData.author_username ?? ""}
