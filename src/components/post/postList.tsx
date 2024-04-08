8ee3b57f9ec64234923df343357cc22c5070bb16
Nielleeeee <jandnllplz@gmail.com>
2024-04-09 04:16:29 +0800

    refactor: added user image

--- src/components/post/postList.tsx
+++ src/components/post/postList.tsx
@@ -6,6 +6,7 @@ import { useAuth } from "@clerk/nextjs";
 import UpdatePost from "./updatePost";
 import KebabDropdown from "../ui/dropdown/kebabDropdown";
 import MediaList from "./mediaList";
+import Image from "next/image";
 
 export default function PostList({ PostData }: any) {
   const { userId } = useAuth();
@@ -17,9 +18,13 @@ export default function PostList({ PostData }: any) {
           <TransitionMoveUp key={index}>
             <div className="relative rounded-md bg-white p-4 my-4 shadow-md">
               <div className="flex mb-4">
-                <div className="flex flex-col w-full">
-                  <h3>{post.author_fullname}</h3>
-                  <DateFormat date={post.xata.createdAt} />
+                <div className="flex flex-row items-center w-full">
+                  <Image src={post.userImage} alt={post.author_username} height={100} width={100} className="rounded-full h-10 w-10 mr-2" />
+
+                  <div>
+                    <h3 className="font-semibold">{post.author_fullname}</h3>
+                    <DateFormat date={post.xata.createdAt} />
+                  </div>
                 </div>
 
                 {userId == post.user_id && (
