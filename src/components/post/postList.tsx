a2890d4963bfbb610555edc26f6d22b093806718
Nielleeeee <jandnllplz@gmail.com>
2024-04-10 02:48:23 +0800

    refactor: format page

--- src/components/post/postList.tsx
+++ src/components/post/postList.tsx
@@ -3,9 +3,9 @@ import { TransitionMoveUp } from "@/components/animation/transition";
 import DateFormat from "@/app/lib/date-format";
 import DeletePost from "@/components/post/deletePost";
 import { useAuth } from "@clerk/nextjs";
-import UpdatePost from "./updatePost";
-import KebabDropdown from "../ui/dropdown/kebabDropdown";
-import MediaList from "./mediaList";
+import UpdatePost from "@/components/post/updatePost";
+import KebabDropdown from "@/components/ui/dropdown/kebabDropdown";
+import MediaList from "@/components/post/mediaList";
 import Image from "next/image";
 
 export default function PostList({ PostData }: any) {
@@ -19,7 +19,13 @@ export default function PostList({ PostData }: any) {
             <div className="relative rounded-md bg-white p-4 my-4 shadow-md">
               <div className="flex mb-4">
                 <div className="flex flex-row items-center w-full">
-                  <Image src={post.userImage} alt={post.author_username} height={100} width={100} className="rounded-full h-10 w-10 mr-2" />
+                  <Image
+                    src={post.userImage}
+                    alt={post.author_username}
+                    height={100}
+                    width={100}
+                    className="rounded-full h-10 w-10 mr-2"
+                  />
 
                   <div>
                     <h3 className="font-semibold">{post.author_fullname}</h3>
