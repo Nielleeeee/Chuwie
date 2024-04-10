213417082abe535cf7d2308049f9106883834e94
Nielleeeee <jandnllplz@gmail.com>
2024-04-11 00:56:07 +0800

    refactor: remove console log

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -2,11 +2,11 @@ import { clerkClient } from "@clerk/nextjs";
 import React from "react";
 
 export default async function SamplePage() {
-  const userInfo = await clerkClient.users.getUser(
-    "user_2dPfCHll2amghRRyU2P7neS8Aih"
-  );
+  // const userInfo = await clerkClient.users.getUser(
+  //   "user_2dPfCHll2amghRRyU2P7neS8Aih"
+  // );
 
-  console.log("User Info: ", userInfo);
+  // console.log("User Info: ", userInfo);
 
   return <section className="h-screen">Sample Page</section>;
 }
