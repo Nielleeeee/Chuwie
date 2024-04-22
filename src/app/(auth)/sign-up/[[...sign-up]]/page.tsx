95344de361be5bb7a7a9bbf1ddfe0f608d56564b
Nielleeeee <jandnllplz@gmail.com>
2024-04-23 02:26:33 +0800

    refactor: UI changes in sign up page

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -31,12 +31,12 @@ export default function Signup() {
                 <SignUp
                   appearance={{
                     baseTheme: shadesOfPurple,
-                    elements: {
-                      socialButtonsBlockButton: "bg-purple-100",
-                      formButtonPrimary:
-                        "bg-purple-400 hover:bg-purple-500 text-sm normal-case",
-                      footerActionLink: "text-purple-700",
-                    },
+                    // elements: {
+                    //   socialButtonsBlockButton: "bg-purple-100",
+                    //   formButtonPrimary:
+                    //     "bg-purple-400 hover:bg-purple-500 text-sm normal-case",
+                    //   footerActionLink: "text-purple-700",
+                    // },
                   }}
                 />
               </ClerkLoaded>
