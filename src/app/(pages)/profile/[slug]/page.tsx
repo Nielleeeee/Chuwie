ac5a71d2a1dff87f9f34599e32715faf0c9b385a
Nielleeeee <jandnllplz@gmail.com>
2024-04-24 03:19:48 +0800

    chore: changed import path

--- src/app/(pages)/profile/[slug]/page.tsx
+++ src/app/(pages)/profile/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import getUser from "@/app/actions/getUser";
+import getUser from "@/app/actions/post/getUser";
 
 // Instead of user id, try finding the username.
 
