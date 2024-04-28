8605b22dc0b77580b01c231b3c564990b2f471aa
Nielleeeee <jandnllplz@gmail.com>
2024-04-29 03:52:15 +0800

    refactor: added new hook for use get all user post

--- src/app/data/get-post.ts
+++ src/app/data/get-post.ts
@@ -1,4 +1,8 @@
-import { getInitialPost, getAllPost } from "@/app/actions/post/getPost";
+import {
+  getInitialPost,
+  getAllPost,
+  getAllUserPost,
+} from "@/app/actions/post/getPost";
 import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 
 export const useGetInitialPost = () => {
@@ -12,6 +16,19 @@ export const useGetAllPost = () => {
   return useInfiniteQuery({
     queryKey: ["allPosts"],
     queryFn: ({ pageParam = 1 }) => getAllPost({ pageParam }) as any,
+    initialPageParam: 1,
+    getNextPageParam: (allPages) => {
+      const hasNextPage = allPages[allPages.length - 1]?.hasNextPage;
+      return hasNextPage ? allPages.length + 1 : undefined;
+    },
+  });
+};
+
+export const useGetAllUserPost = (username: string) => {
+  return useInfiniteQuery({
+    queryKey: ["allPosts"],
+    queryFn: ({ pageParam = 1 }, pageSize = 3) =>
+      getAllUserPost({ pageParam }, pageSize, username) as any,
     initialPageParam: 1,
     getNextPageParam: (allPages) => {
       const hasNextPage = allPages[allPages.length - 1]?.hasNextPage;
