97f20264c7cfb33b5b2b0bb31467f42c02bb92e3
Nielleeeee <jandnllplz@gmail.com>
2024-04-30 04:14:54 +0800

    refactor: added query client and main container

--- src/app/(pages)/profile/[username]/page.tsx
+++ src/app/(pages)/profile/[username]/page.tsx
@@ -2,12 +2,20 @@ import { notFound } from "next/navigation";
 import getUser from "@/app/actions/user/getUser";
 import UserInfo from "@/components/user/userInfo";
 import UserPost from "@/components/post/userPost";
+import { MainContainer } from "@/components/container/container";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
 
 export default async function ProfilePage({
   params,
 }: {
   params: { username: string };
 }) {
+  const queryClient = new QueryClient();
+
   const userInfo = await getUser(params.username);
 
   if (!userInfo) {
@@ -16,9 +24,13 @@ export default async function ProfilePage({
 
   return (
     <main className="min-h-screen bg-slate-700">
-      <UserInfo {...userInfo} />
+      <MainContainer>
+        <UserInfo {...userInfo} />
 
-      <UserPost username={userInfo.username || ""} />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <UserPost username={userInfo.username || ""} />
+        </HydrationBoundary>
+      </MainContainer>
     </main>
   );
 }
