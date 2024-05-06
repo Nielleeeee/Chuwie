9f1c775edec824f14dc7da356493e3bdd64e935b
Nielleeeee <jandnllplz@gmail.com>
2024-05-07 02:54:45 +0800

    refactor: added aws s3 handler for uploading a file

--- src/app/api/create-post/route.ts
+++ src/app/api/create-post/route.ts
@@ -1,19 +1,30 @@
 import type { NextRequest } from "next/server";
-import { v2 as cloudinary } from "cloudinary";
 import { getXataClient } from "@/xata";
 import { currentUser } from "@clerk/nextjs";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import crypto from 'crypto'
 
-cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
-});
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3Client = new S3Client([
+  {
+    region,
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  },
+]);
+
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const { content, media } = body;
 
-  const { image, video } = media;
+  const { content, media } = body;
 
   const xataClient = getXataClient();
 
@@ -21,62 +32,42 @@ export async function POST(req: NextRequest) {
   const author_id = (user?.publicMetadata.user_id as string) || "";
 
   try {
-    const mediaUrl: {
+    const mediaName: {
       secure_url: string;
       public_id: string;
       format?: string;
     }[] = [];
 
-    // Upload image to cloudinary
-    await Promise.all(
-      image.map(async (fileData: any) => {
-        const buffer = Buffer.from(fileData.data);
+    const uploadMediaS3 = await Promise.all(
+      media.map(async (media: CombinedMedia) => {
+        const mediaBuffer = Buffer.from(media.data);
 
-        return new Promise((resolve, reject) => {
-          cloudinary.uploader
-            .upload_stream({ tags: ["chuwie"] }, function (error, result) {
-              if (error) {
-                reject(error);
-                return;
-              }
-              mediaUrl.push({
-                secure_url: result?.secure_url as string,
-                public_id: result?.public_id as string,
-              });
-              resolve(result);
-            })
-            .end(buffer);
-        });
+        const fileName = generateFileName()
+
+        const params = {
+          Bucket: bucketName,
+          Body: mediaBuffer,
+          Key: fileName,
+          ContentType: media.mimetype,
+        };
+
+        try {
+          await s3Client.send(new PutObjectCommand(params));
+
+          return { success: true, media: fileName };
+        } catch (error) {
+          console.error(`Error uploading ${media.filename} to S3:`, error);
+          return { success: false, media: fileName };
+        }
       })
     );
 
-    await Promise.all(
-      video.map(async (fileData: any) => {
-        const buffer = Buffer.from(fileData.data);
-
-        return new Promise((resolve, reject) => {
-          cloudinary.uploader
-            .upload_stream({ tags: ["chuwie"] }, function (error, result) {
-              if (error) {
-                reject(error);
-                return;
-              }
-              mediaUrl.push({
-                secure_url: result?.secure_url as string,
-                public_id: result?.public_id as string,
-                format: result?.format as string,
-              });
-              resolve(result);
-            })
-            .end(buffer);
-        });
-      })
-    );
+    console.log(uploadMediaS3);
 
     // Insert Data to xata database
     const postData = {
       content,
-      media: mediaUrl,
+      media: uploadMediaS3.map((item) => item.media),
       author: author_id,
     };
 
@@ -99,3 +90,26 @@ export async function POST(req: NextRequest) {
     });
   }
 }
+
+// Upload image to cloudinary
+// await Promise.all(
+//   image.map(async (fileData: any) => {
+//     const buffer = Buffer.from(fileData.data);
+
+//     return new Promise((resolve, reject) => {
+//       cloudinary.uploader
+//         .upload_stream({ tags: ["chuwie"] }, function (error, result) {
+//           if (error) {
+//             reject(error);
+//             return;
+//           }
+//           mediaUrl.push({
+//             secure_url: result?.secure_url as string,
+//             public_id: result?.public_id as string,
+//           });
+//           resolve(result);
+//         })
+//         .end(buffer);
+//     });
+//   })
+// );
