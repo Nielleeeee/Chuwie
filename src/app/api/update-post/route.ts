508034fb679c4e1b062a7df8f140f903eb523caf
Nielleeeee <jandnllplz@gmail.com>
2024-05-13 01:12:30 +0800

    refactor: added handler for video and changed cloudinary to aws s3

--- src/app/api/update-post/route.ts
+++ src/app/api/update-post/route.ts
@@ -1,13 +1,31 @@
 import type { NextRequest } from "next/server";
 import { v2 as cloudinary } from "cloudinary";
 import { getXataClient } from "@/xata";
-import { currentUser } from "@clerk/nextjs";
+import {
+  S3Client,
+  DeleteObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { signedUrl } from "@/app/actions/aws/signedUrl";
+import crypto from "crypto";
 
-cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
-});
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3Client = new S3Client([
+  {
+    region,
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  },
+]);
+
+const generateFileName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString("hex");
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -17,46 +35,69 @@ export async function POST(req: NextRequest) {
   const xataClient = getXataClient();
 
   try {
-    const mediaUrl: { secure_url: string; public_id: string }[] = [];
+    const mediaObject: {
+      fileName: string;
+      type: string;
+      url: string;
+    }[] = [];
 
     // // Delete updated image in cloudinary
     if (toDelete?.length !== 0) {
-      toDelete?.map(async (publicId: any) => {
-        const deleteImage = await cloudinary.uploader.destroy(publicId);
+      toDelete?.map(async (media: any) => {
+        const deleteParams = {
+          Bucket: bucketName,
+          Key: media,
+        };
+
+        await s3Client.send(new DeleteObjectCommand(deleteParams));
       });
     }
 
-    // Upload file to cloudinary
-    if (media.image.length !== 0) {
+    // Upload media to S3
+    if (media.length !== 0) {
       await Promise.all(
-        media.image.map(async (fileData: any) => {
-          const buffer = Buffer.from(fileData.data);
+        media.map(async (media: CombinedMedia) => {
+          const mediaBuffer = Buffer.from(media.data);
 
-          return new Promise((resolve, reject) => {
-            cloudinary.uploader
-              .upload_stream({ tags: ["chuwie"] }, function (error, result) {
-                if (error) {
-                  reject(error);
-                  return;
-                }
-                mediaUrl.push({
-                  public_id: result?.public_id as string,
-                  secure_url: result?.secure_url as string,
-                });
-                resolve(result);
-              })
-              .end(buffer);
-          });
+          const fileName = generateFileName();
+
+          const params = {
+            Bucket: bucketName,
+            Body: mediaBuffer,
+            Key: fileName,
+            ContentType: media.mimetype,
+          };
+
+          try {
+            await s3Client.send(new PutObjectCommand(params));
+
+            const mediaUrl = await signedUrl(fileName);
+
+            mediaObject.push({
+              fileName,
+              type: media.mimetype,
+              url: mediaUrl,
+            });
+
+            return {
+              success: true,
+              media: fileName,
+              mediaType: media.mimetype,
+            };
+          } catch (error) {
+            console.error(`Error uploading ${media.filename} to S3:`, error);
+            return { success: false, media: fileName };
+          }
         })
       );
     }
 
     const filteredMedia = currentMedia.filter(
       (mediaItem) =>
-        !toDelete?.some((publicId) => publicId === mediaItem.public_id)
+        !toDelete?.some((fileName) => fileName === mediaItem.fileName)
     );
 
-    const updatedMedia = JSON.stringify([...filteredMedia, ...mediaUrl]);
+    const updatedMedia = JSON.stringify([...filteredMedia, ...mediaObject]);
 
     const updatePostResult = await xataClient.transactions.run([
       {
