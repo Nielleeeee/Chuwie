ab779d87394880d5f54ee526b462bb68514b8485
Nielleeeee <jandnllplz@gmail.com>
2024-05-17 01:52:49 +0800

    refactor: changed the timestamp to string

--- src/app/api/update-post/route.ts
+++ src/app/api/update-post/route.ts
@@ -73,7 +73,7 @@ export async function POST(req: NextRequest) {
               fileName,
               type: media.mimetype,
               url: mediaUrl,
-              timestamp: new Date(),
+              timestamp: new Date().toISOString(),
             });
 
             return {
