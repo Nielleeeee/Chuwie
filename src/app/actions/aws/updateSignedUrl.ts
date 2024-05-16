58d41d6d1cb8c240ff5eb0ddc3c97d5299b2c5c1
Nielleeeee <jandnllplz@gmail.com>
2024-05-17 01:52:07 +0800

    refactor: update the media in the database

--- src/app/actions/aws/updateSignedUrl.ts
+++ src/app/actions/aws/updateSignedUrl.ts
@@ -1,19 +1,44 @@
+"use server";
+
+import { getXataClient } from "@/xata";
 import { signedUrl } from "./signedUrl";
 
-export const updateSignedUrl = async (mediaItems: MediaItem[]) => {
-  const sevenDaysAgo = new Date();
-  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+export const updateSignedUrl = async (
+  mediaItems: MediaItem[],
+  postID: string
+) => {
+  const xataClient = getXataClient();
 
-  return Promise.all(
-    mediaItems.map(async (mediaItem: MediaItem) => {
-      if (new Date(mediaItem.timestamp) > sevenDaysAgo) {
-        const updatedUrl = await signedUrl(mediaItem.fileName);
-        return {
-          ...mediaItem,
-          url: updatedUrl,
-        };
-      }
-      return mediaItem;
-    })
-  );
+  try {
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+    const updatedMedia = await Promise.all(
+      mediaItems.map(async (mediaItem: MediaItem) => {
+        if (new Date(mediaItem.timestamp) > sevenDaysAgo) {
+          const updatedUrl = await signedUrl(mediaItem.fileName);
+
+          return {
+            ...mediaItem,
+            timestamp: new Date().toISOString(),
+            url: updatedUrl,
+          };
+        }
+
+        return mediaItem;
+      })
+    );
+
+    // Update xata database
+    const updateDb = await xataClient.db.Post.update(postID, {
+      media: JSON.stringify(updatedMedia),
+    });
+
+    console.log(updateDb);
+
+    return updatedMedia;
+  } catch (error) {
+    console.error("Failed to update signed url: ", error);
+    throw error;
+  }
 };
