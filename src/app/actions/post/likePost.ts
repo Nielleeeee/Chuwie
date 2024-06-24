071509d33567f235aaf8da904685ba66309618b7
Nielleeeee <jandnllplz@gmail.com>
2024-06-25 05:04:15 +0800

    refactor: added isliked checker

--- src/app/actions/post/likePost.ts
+++ src/app/actions/post/likePost.ts
@@ -6,7 +6,31 @@ export const likePost = async ({ post_id, user_id }: LikePostParams) => {
   try {
     const xataClient = getXataClient();
 
-    await xataClient.db.Like.create({ post_id, user_id });
+    if (!user_id) {
+      throw new Error("User ID is required to like a post.");
+    }
+
+    const isLiked = await xataClient.db.Like.filter({
+      "post_id.id": post_id,
+      "user_id.id": user_id,
+    }).getFirst();
+
+    if (!isLiked) {
+      await xataClient.db.Like.create({ post_id, user_id });
+    }
+
+    return { status: true, error: null };
+  } catch (error) {
+    console.error(error);
+    return { status: null, error: error };
+  }
+};
+
+export const removelikePost = async ({ likeId }: { likeId: string }) => {
+  try {
+    const xataClient = getXataClient();
+
+    await xataClient.db.Like.delete(likeId);
 
     return { status: true, error: null };
   } catch (error) {
